514e20534ad99c499fd86f98a67df90799f4cd9d
FilippChubukov <filipp.chubukov.98@mail.ru>
2017-05-28 23:44:29 +0400

    Changes in main.kt
    add communication with user

---
+++ RBT/main.kt
@@ -0,0 +1,70 @@
+package RBT
+
+fun main(args: Array<String>) {
+
+    val rbt = RBT<Int, Char>()
+
+    println("f to find")
+    println("d to delete")
+    println("p to print")
+    println("key to insert")
+
+    while (true) {
+
+        val operation = readLine()
+
+        when (operation) {
+
+            "f" -> {
+
+                print("Key to find: ")
+
+                val key = readLine()?.toIntOrNull()
+
+                if (key != null) {
+
+                    if (rbt.find(key) != null) println("Key found")
+
+                    else println("Key not found")
+
+                } else println ("You didn't give key.")
+
+            }
+
+            "p" -> Printer<Int, Char>().printTree(rbt)
+
+
+            "d" -> {
+
+                println("Key to delete: ")
+
+                val key = readLine()?.toIntOrNull()
+
+                if (key != null) rbt.delete(key)
+
+                else println ("You didn't give key.")
+
+            }
+
+            "q" -> return
+
+            else -> {
+
+                val key = operation?.toIntOrNull()
+
+                if (key != null) {
+
+                    val value: Char = 'a'
+
+                    if (rbt.find(key) != null) {
+
+                        println("This key already in tree.")
+
+                    } else rbt.insert(key, value)
+
+                } else println ("You didn't give key.")
+
+            }
+        }
+    }
+}
